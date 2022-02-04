855a3b9b9849cf6e484f3c45c46876a9a3d5019d
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-02-04 23:52:17 +0100

    feat: get userID from URL

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap
+    .subscribe( paramMap => {
+      this. = paramMap.get('');
+    })
   }
 
 }
